a5d2724f83b792b85ce34ece9087abbea0a9a2bb
Musaab Siddiqui <mushi251@hotmail.com>
2020-12-27 13:06:00 -0500

    adding the files to the repo

--- script.js
+++ script.js
@@ -1,6 +1,22 @@
-document.body.onkeyup = function(event) {
-  console.log(event.keyCode);
-  if (event.keyCode == 68)
-  // change the class of body 
-  document.body.classList.toggle("dark");
+const toggleSwitch = document.querySelector('.theme-switch input[type="checkbox"]');
+const currentTheme = localStorage.getItem('theme');
+
+if (currentTheme) {
+    document.documentElement.setAttribute('data-theme', currentTheme);
+  
+    if (currentTheme === 'dark') {
+        toggleSwitch.checked = true;
+    }
 }
+
+function switchTheme(e) {
+    if (e.target.checked) {
+        document.documentElement.setAttribute('data-theme', 'dark');
+        localStorage.setItem('theme', 'dark');
+    }
+    else {        document.documentElement.setAttribute('data-theme', 'light');
+          localStorage.setItem('theme', 'light');
+    }    
+}
+
+toggleSwitch.addEventListener('change', switchTheme, false);
